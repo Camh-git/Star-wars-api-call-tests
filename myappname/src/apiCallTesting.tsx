6f35e7072c7a495331bb64c1ee70216008fb921c
Camh-git <cameronhillier1@gmail.com>
2023-09-19 22:08:31 +0100

    feat: testing
    added some simple testing (rendering and if some data is present)
    still need to display this data on the screen.
    
    commented out all of the weirdly imported code, hopefully this will give a good idea of what I was trying to do

--- myappname/src/apiCallTesting.tsx
+++ myappname/src/apiCallTesting.tsx
@@ -1,18 +1,58 @@
 import { useEffect } from "react";
+import { useState } from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+//import "@testing-library/jest-dom";
+import { rest } from "msw";
+import { setupServer } from "msw/node";
+import * as sampleData from "./returnedData.json";
+
+/*
+const server = setupServer(
+  rest.get("https://swapi.dev/api/people/1", (req, res, ctx) => {
+    return res(ctx.json(sampleData));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+*/
+
+/*
+test("Make sure luke skywalker is the first person returned", async () => {
+  render(<CallApi />);
+  //simple render test
+  const header = screen.getByText("Char data:");
+  expect(header).toBeInTheDocument();
+  //check some data rendered(in this case luke's birth year)
+  const dataSample = screen.getByText("19BBY");
+  expect(dataSample).toBeInTheDocument();
+});
+*/
+
 const CallApi: React.FC = () => {
+  const [resp, setResp] = useState({});
   let response = "";
   useEffect(() => {
     const list = async () => {
       const result = await fetch("https://swapi.dev/api/people/1");
       result.json().then((json) => {
         response = json;
+        setResp(response);
         console.log("data:");
         console.log(response);
       });
     };
     list();
   }, []);
-  return <></>;
+  return (
+    <>
+      <h2>Char data:</h2>
+      {resp === "" && "Loading..."}
+      {resp !== "" && <p>{response}</p>}
+    </>
+  );
 };
 export default CallApi;
 //See returnedData.json for a copy of what this returned
