103392a79dbda62c64c267e2bb56bb4294704282
Camh-git <cameronhillier1@gmail.com>
2023-09-19 21:08:07 +0100

    Revert "Feat:MSW added"
    This reverts commit edf8f00db3c2f0a99cf21433eb13b59a2937289a.
    it caused an error:
    ERROR in ../../../../node_modules/@mswjs/interceptors/lib/interceptors/ClientRequest/NodeClientRequest.js 184:13-28
    Module not found: Error: Can't resolve 'http' in 'C:\Users\camer\node_modules\@mswjs\interceptors\lib\interceptors\ClientRequest'
    
    BREAKING CHANGE: webpack < 5 used to include polyfills for node.js core modules by default.
    This is no longer the case. Verify if you need this module and configure a polyfill for it.
    
    If you want to include a polyfill, you need to:
    	- add a fallback 'resolve.fallback: { "http": require.resolve("stream-http") }'
    	- install 'stream-http'
    If you don't want to include a polyfill, you can use an empty module like this:
    	resolve.fallback: { "http": false }

--- myappname/src/apiCallTesting.tsx
+++ myappname/src/apiCallTesting.tsx
@@ -1,25 +1,4 @@
 import { useEffect } from "react";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
-import "@testing-library/jest-dom";
-import { rest } from "msw";
-import { setupServer } from "msw/node";
-import * as sampleData from "./returnedData.json";
-
-const server = setupServer(
-  rest.get("https://swapi.dev/api/people/1", (req, res, ctx) => {
-    return res(ctx.json(sampleData));
-  })
-);
-
-beforeAll(() => server.listen());
-afterEach(() => server.resetHandlers());
-afterAll(() => server.close());
-
-test("Make sure luke skywalker is the first person returned", async () => {
-  render(<CallApi />);
-});
-
 const CallApi: React.FC = () => {
   let response = "";
   useEffect(() => {
